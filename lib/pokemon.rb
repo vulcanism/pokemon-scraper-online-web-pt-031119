aae28d7e86b9a2df3ef1cb163f07acb407a8c949
Savannah C <sgcorrell@gmail.com>
2019-05-23 14:55:24 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -18,7 +18,7 @@ class Pokemon
     new_pokemon.id = pokemon[0][0]
     new_pokemon.name = pokemon[0][1]
     new_pokemon.type = pokemon[0][2]
-    new_pokemon
+    return new_pokemon
   end
   
 end
